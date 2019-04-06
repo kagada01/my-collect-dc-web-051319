5421221a63a3d43f6fa5115446a1c1fb668ea138
Dan Kagan <dankagan7@gmail.com>
2019-04-06 16:29:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,4 +10,4 @@ i = 0
     storageOne
 end
 
-my_collect(array) { |name| storageTwo.push(name.split(" ").first storageTwo)}
+my_collect(array[i]) { |name| storageTwo.push(name.split(" ").first storageTwo)}
