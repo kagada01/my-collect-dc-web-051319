d5b3f123b210bcee5234557f1fe70364171d45e0
Dan Kagan <dankagan7@gmail.com>
2019-04-06 16:27:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+storageOne = []
+storageTwo = []
+i = 0
+  while (i < array.length) do 
+    storageOne.push(array[i].upcase)
+    yield array[i] { |name| storageTwo.push(name.split(" ").first storageTwo)}
+    i += 1
+  end
+    storageOne
+end
 
