840d89281c9e1d2947920fac3798e28eeb448ace
Dan Kagan <dankagan7@gmail.com>
2019-04-06 16:27:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ storageTwo = []
 i = 0
   while (i < array.length) do 
     storageOne.push(array[i].upcase)
-    yield array[i] { |name| storageTwo.push(name.split(" ").first storageTwo)}
+    yield(array[i]) { |name| storageTwo.push(name.split(" ").first storageTwo)}
     i += 1
   end
     storageOne
